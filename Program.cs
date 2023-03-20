eb0520272755310f4a36fa123f2cc9c5df652244
livijoskoda2000 <116301032+livijoskoda2000@users.noreply.github.com>
2023-03-20 09:31:17 +0100

    Update Program.cs
    dodana jos jedna linija

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp22
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo"); 
+            Console.WriteLine("Novo2"); 
         }
     }
 }
